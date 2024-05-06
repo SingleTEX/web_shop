6202d0b6689d7e37e16c4bedb634f1fcc366b600
SingleTEX <120369283+SingleTEX@users.noreply.github.com>
2024-05-06 14:15:05 +0200

    main changrs

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var burgerMenu = document.getElementById('burger-menu');
-    var dropdownContent = document.getElementById('dropdown-content');
-  
-    burgerMenu.addEventListener('click', function() {
-      dropdownContent.classList.toggle('active');
-    });
-  });
