d62f38e0e45dce5a023d8cd63d57a429a4594158
SingleTEX <120369283+SingleTEX@users.noreply.github.com>
2024-05-13 09:24:29 +0200

    to have thi updated

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+header.innerHTML = `
+    <h1>Oktika</h1>
+    <div class="navbar">
+        <a href="index.html" class="active">Store</a>
+        <a href="cart.html">Cart</a>
+        <a href="profile.html">Profile</a>
+        <a href="sign_up.html" class="right">Log in</a>
+        <div class="burger-menu" id="click">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="three"></div>
+            <nav class="dropdown-content">
+        <a href="index.html" class="active">Store</a>
+        <a href="cart.html">Cart</a>
+        <a href="profile.html">Profile</a>
+        <a href="sign_up.html" class="right">Log in</a>
+            </nav>
+        </div>
+    </div>
+`;
+
+footer.innerHTML = ` 
+    <h2>Light studio</h2>
+    <p>Copyright © 2024 All rights reserved.</p>
+`;
+
+
